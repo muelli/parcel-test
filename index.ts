79a455f8883da4972b491eaab233eb58e484264f
Tobias Mueller <muelli@cryptobitch.de>
2019-10-23 23:48:21 +0200

    adding libsodium and tweet-nacl
    This does not work in IE due to Promise not defined.

--- index.ts
+++ index.ts
@@ -2,6 +2,17 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+import * as libsodium from "libsodium-wrappers";
+import * as naclutil from "tweetnacl-util";
+
+export function sodium_init() {
+    return libsodium.ready;
+}
+
+export const utf8enc = naclutil.encodeUTF8;
+export const utf8dec = naclutil.decodeUTF8;
+
+                        
 import {Elm} from "./Main.elm";
 
 const div = document.getElementById("main");
