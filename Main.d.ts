299c9d73eca8544e262f5c1e511e34a8357e1644
Tobias Mueller <muelli@cryptobitch.de>
2019-10-25 10:05:35 +0200

    typescript: Rename the interface of the Elm module
    this is how elm-typescript-interop would render it.

--- Main.d.ts
+++ Main.d.ts
@@ -1,11 +1,11 @@
 export namespace Elm {
-    namespace LoginApp {
+    namespace Main {
         export function init(options: {
             node?: HTMLElement | null;
             flags: {
                 credentials: { username: string; password: string } | null;
                 translations: { de: any; en: any } | null;
             } | null
-        }): Elm.LoginApp.App;
+        }): Elm.Main.App;
     }
 }
